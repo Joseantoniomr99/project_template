1084ccfad8d76377e583dd07e84a751f45ef17d9
emasto2111 <80511538+emasto2111@users.noreply.github.com>
2022-02-03 23:48:30 +0100

    bitr
    analisis de cuantos genes de humanos y covid tenemos si usamos bitr

---
+++ code/analisisBit.R
@@ -0,0 +1,27 @@
+protTraduc<- read.csv("ProteomaTraducido.csv")
+network.table <- openxlsx::read.xlsx("Prey_Lookup_Table.xlsx")
+names(network.table)<- c(network.table[1,1],network.table[1,2],network.table[1,3],
+                         network.table[1,4],network.table[1,5],network.table[1,6],
+                         network.table[1,7],network.table[1,8],network.table[1,9],
+                         network.table[1,10],network.table[1,11],network.table[1,12])
+network.table<- network.table[-1,]
+
+#Comprobamos cuantos valores de proteinas de nuestra network table se encuentra en nuestro proteoma traducido
+# Vemos que solo encontramos 18 de las proteinas, de un total de 332 proteinas. 
+ain<- network.table$PreyGeneName %in% protTraduc$protein1
+v<-c()
+for(i in 1:length(ain)){
+  if(ain[i]==TRUE){
+    v<- c(v,i)
+  }else{
+    i=i+1
+  }
+  
+}
+ # Añadimos en dos vectores los valores de esas posiciones, y vemos que solo conseguimos 2 genes covid.
+prot<- c()
+cov<- c()
+for(i in 1:length(v)){
+  prot<-c(prot, network.table$PreyGeneName[i])
+  cov<- c(cov, network.table$Bait[i])
+}
